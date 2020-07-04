0776ea2c444ec1fc1f5f70020055cf7cd13cbfb0
MD SABBIR KHAN <64284482+md-sabbir-khan@users.noreply.github.com>
2020-07-05 03:07:14 +0530

    updated

--- mail.py
+++ mail.py
@@ -1,21 +1,21 @@
 import smtplib 
 
-	# creates SMTP session 
-	s = smtplib.SMTP('smtp.gmail.com', 587) 
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
 
-	# start TLS for security 
-	s.starttls()   
+# start TLS for security 
+s.starttls()   
 
-	# Authentication 
-	s.login("sabbirisdon@gmail.com", "*tiktaktoe#")
+# Authentication 
+s.login("sabbirisdon@gmail.com", "*tiktaktoe#")
  
-	# message to be sent 
-	message = "Hey Developer, Finally the model is successfully trained. "
+# message to be sent 
+message = "Hey Developer, Finally the model is successfully trained. "
 
-	# sending the mail 
+# sending the mail 
 
-	s.sendmail("sabbirisdon@gmail.com", "khan.mdsabbir@gmail.com", message)
-	# terminating the session 
+s.sendmail("sabbirisdon@gmail.com", "khan.mdsabbir@gmail.com", message)
+# terminating the session 
 
-	s.quit()
+s.quit()
 
